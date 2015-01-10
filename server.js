bcf053fec154c6bd2be07272bd7ef648d0be0e0d
unknown <aceeristudios@gmail.com>
2015-01-09 17:50:55 -0800

    commit

--- server.js
+++ server.js
@@ -174,6 +174,7 @@ var WebServer = function() {
 		self.app.get('/arenas/remove/:id', function(req, res){
 			var found = false;
 			for (i = 0; i < self.arenas.length; i++) {
+				console.log(self.arenas[i].arenaid, req.params.id);
 				if (self.arenas[i].arenaid == req.params.id) {
 					self.arenas.splice(i, 1);
 					var found = true;
